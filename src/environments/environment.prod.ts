d5c840682a147c2748ee50c87455f9dab0cf04d4
Nikita Kuznetsov <skateaddict@gmail.com>
2018-08-27 03:09:11 -0700

    add firebase config to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC3V6PtqkgYpyEpFl-AIzaSyC3V6PtqkgYpyEpFl-UV66FWuY5CqOuiMI',
+    authDomain: 'yabba-tabba-doo.firebaseapp.com',
+    databaseURL: 'https://yabba-tabba-doo.firebaseio.com',
+    projectId: 'yabba-tabba-doo',
+    storageBucket: 'yabba-tabba-doo.appspot.com',
+    messagingSenderId: '770260837317'
+  }
 };
